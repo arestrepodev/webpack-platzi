409ab4fa893e53f9b71d13d722640b2f6cc9ed9a
Arnold Restrepo <restrepo_arnold@outlook.com>
2019-11-17 21:54:55 -0500

    Plugins

---
+++ plugins/webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const MiniCSSExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const config = {
+  mode: 'development',
+  entry: path.resolve(__dirname, 'src', 'js', 'index.js'),
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: MiniCSSExtractPlugin.loader
+          },
+          'css-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Plugins'
+    }),
+    new MiniCSSExtractPlugin({
+      filename: 'css/[name].css'
+    })
+  ],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'js/bundle.js'
+  },
+  watch: true
+};
+
+module.exports = config;
