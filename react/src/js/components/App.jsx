548fcd126ea50245ca7c31f1421612c32dab551c
Arnold Restrepo <restrepo_arnold@outlook.com>
2019-11-21 20:03:50 -0500

    React Exercise

---
+++ react/src/js/components/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import data from "./data.json"
+
+const App = () => {
+  console.log(data);
+  
+  return(
+    <div>
+      <p>Que linda app hecha en React</p>
+    </div>
+  )
+}
+
+export default App;
---
+++ react/webpack.config.js
@@ -0,0 +1,46 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+
+const config = {
+  mode: 'development',
+  entry: path.resolve(__dirname, 'src', 'js', 'index.js'),
+  devServer: {
+    open: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        use: ['babel-loader'], 
+        exclude: "/nonde_modules/"
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'React',
+      minify: {
+        collapseWhitespace: true
+      },
+      template: path.resolve(__dirname, "src/index.html")
+    })
+  ],
+  resolve: {
+    alias: {
+      Node: path.resolve(__dirname, "node_modules")
+    },
+    extensions: [".js", ".jsx"]
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'js/bundle.js'
+  },
+};
+
+module.exports = config;
