cf1af36b3731a2ba18b79439bd9940778b7dc6ee
Arnold Restrepo <restrepo_arnold@outlook.com>
2019-11-25 10:05:02 -0500

    URL Loader

---
+++ url-loader/src/js/components/App.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import data from "./data.json"
+import Loader from "./Loader"
+import logo from "../../img/platzi.png";
+import video from "../../video/que-es-core.mp4";
+
+const App = () => {
+  const [loaderList, setLoaderList] = useState([])
+  
+  const handleClick = (event) => {
+    event.preventDefault();
+    setLoaderList(data.loaders)
+  }
+
+  return(
+    <article>
+      <p>URL Loader</p>
+      <img src={logo} alt="Logo" width="40"/>
+      <video src={video} width="360" controls autoplay poster={logo}></video>
+      <ul>
+        {loaderList.map(item => {
+          return (
+            <Loader {...item} key={item.id}/>
+          )
+        })}
+      </ul>
+      <button type="button" onClick={handleClick}>Mostrar lo aprendido</button>
+    </article>
+  )
+}
+
+export default App;
---
+++ url-loader/webpack.config.js
@@ -0,0 +1,55 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+
+const config = {
+  mode: 'development',
+  entry: path.resolve(__dirname, 'src', 'js', 'index.js'),
+  devServer: {
+    open: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        use: ['babel-loader'], 
+        exclude: "/nonde_modules/"
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.jpg|png|gif|woff|eot|ttf|svg|mp4|webm$/,
+        use:{
+          loader: 'url-loader',
+          options: {
+            limit: 90000
+          }
+        }
+      }
+    ]
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'React',
+      minify: {
+        collapseWhitespace: true
+      },
+      template: path.resolve(__dirname, "src/index.html")
+    })
+  ],
+  resolve: {
+    alias: {
+      Node: path.resolve(__dirname, "node_modules")
+    },
+    extensions: [".js", ".jsx"]
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'js/bundle.js'
+  },
+};
+
+module.exports = config;
