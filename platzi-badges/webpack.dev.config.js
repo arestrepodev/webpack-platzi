1b9c1c813ace2862a23263850bed348679364300
Arnold Restrepo <restrepo_arnold@outlook.com>
2019-12-05 17:30:01 -0500

    Initial Prod Webpack Platzi-badges

--- platzi-badges/webpack.dev.config.js
+++ platzi-badges/webpack.dev.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractTextPlugin = require('mini-css-extract-plugin');
 const webpack = require('webpack');
 
 module.exports = {
